de1d208792d3ccf7cf2b4fd20e59422b228d8113
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2025-05-14 03:09:43 +0700

    add view schedules

--- app/Filament/Resources/ScheduleResource.php
+++ app/Filament/Resources/ScheduleResource.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources;
 
 use App\Filament\Resources\ScheduleResource\Pages;
 use App\Filament\Resources\ScheduleResource\RelationManagers;
+use App\Filament\Resources\SchedulesResource\Pages\ViewSchedules;
 use App\Models\Schedule;
 use App\Models\ScheduleTemplate;
 use Filament\Forms;
@@ -70,6 +71,7 @@ class ScheduleResource extends Resource
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
                 Tables\Actions\DeleteAction::make(),
             ])
@@ -91,6 +93,7 @@ class ScheduleResource extends Resource
     public static function getPages(): array
     {
         return [
+            'view' => ViewSchedules::route('/{record}'),
             'index' => Pages\ListSchedules::route('/'),
             'edit' => Pages\EditSchedule::route('/{record}/edit'),
         ];
