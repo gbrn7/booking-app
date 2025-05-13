b8bce45be90cade3a6e02be80faa89537ab0d95f
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2025-05-14 03:09:55 +0700

    add view schedules

---
+++ app/Filament/Resources/ScheduleResource/Pages/ViewSchedules.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\SchedulesResource\Pages;
+
+use App\Filament\Resources\ScheduleResource;
+use Filament\Resources\Pages\ViewRecord;
+
+class ViewSchedules extends ViewRecord
+{
+    protected static string $resource = ScheduleResource::class;
+}
