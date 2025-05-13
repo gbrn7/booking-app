6ccc2d4cb66ed61509e7b712341034f77a0d6900
Muhammad Rayhan Gibran <mraygbrn@gmail.com>
2025-05-14 02:49:07 +0700

    fix middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web(append: [
+        $middleware->validateCsrfTokens(
+            except: ['api/webhook'],
+        )->web(append: [
             \RealRashid\SweetAlert\ToSweetAlert::class,
         ]);
     })
